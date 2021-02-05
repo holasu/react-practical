cde96d2d13eab9f2bfb40448c47ad2e17c31ba1a
holasu <hola.sudheera@gmail.com>
2021-02-05 22:39:20 +0530

    product list created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import ProductData from "./ProductData"
+import Product from "./Product"
+
+
+function App(){
+  
+      const productCommponents = ProductData.map( item => < Product  key={item.id} products={item}/>)
+
+      return(
+        <div className="App">
+              {productCommponents}
+        </div>
+      );
 }
 
 export default App;
